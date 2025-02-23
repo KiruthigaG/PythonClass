e4960cdc528e9c3f9cbf680a816d55ac83ed7534
KiruthigaG <86771101+KiruthigaG@users.noreply.github.com>
2025-02-23 20:30:29 +0530

    Q3_5-Answer
       5.	Extract Every Second Character
        Given a string, print every second character using slicing.

---
+++ Q5.py
@@ -0,0 +1,6 @@
+# 5.	Extract Every Second Character
+# Given a string, print every second character using slicing.
+
+str = "this is to test the string slicing"
+print("Every second character from the string below:")
+print(str[0::2])
