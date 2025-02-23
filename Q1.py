e6d83755e743be1f33c2d5bd28c08089f5d58f33
KiruthigaG <86771101+KiruthigaG@users.noreply.github.com>
2025-02-23 20:35:05 +0530

    Q2_1-Answer
    Write a Python program to swap two variables without using a temporary variable.

--- Q1.py
+++ Q1.py
@@ -1,6 +1,19 @@
-# 1.	Reverse a String
-# Write a program that takes a string as input and prints the reversed string using slicing.
-str='Reverse a String'
-print('String before reversing: ' +str)
-print('String after reversing: ' +str[::-1])
+# 1.Write a Python program to swap two variables without using a temporary variable.
 
+# With integer
+a=5
+b=10
+print("Before swapping: ")
+print(a,b)
+# Swapping action
+a,b=b,a
+print("After swapping: ")
+print(a,b)
+# With string
+a="Hello"
+b="Morning"
+print("Before swapping the string:")
+print(a,b)
+a,b=b,a
+print("After swapping the string:")
+print(a,b)
