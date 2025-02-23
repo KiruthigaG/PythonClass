2b9191e90bff47193d855503b89848dc8b4a21e1
KiruthigaG <86771101+KiruthigaG@users.noreply.github.com>
2025-02-23 20:25:42 +0530

    Q2_2-Answer
     Create a program that takes a list of numbers and prints the sum, average, maximum, and minimum of the list.

---
+++ Q2.py
@@ -0,0 +1,9 @@
+# Create a program that takes a list of numbers and prints the sum, average, maximum, and minimum of the list.
+
+list1 =[3,5,7,8,2,10]
+print("Sum value of the list: " +str(sum(list1)))
+print("Average value of the list: " +str(sum(list1)/len(list1)))
+print("Maximum value of the list: " +str(max(list1)))
+print("Minimum value of the list: " +str(min(list1)))
+
+
