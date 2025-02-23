e416ca7883c24531c7494352011bf6294b464b11
KiruthigaG <86771101+KiruthigaG@users.noreply.github.com>
2025-02-23 20:28:51 +0530

    Q3_3-Answer
    3.	Check for Palindrome
        Write a program to check whether the given string is a palindrome or not using slicing.

--- Q3.py
+++ Q3.py
@@ -1,6 +1,20 @@
-#Write a Python program to check if a number is prime.
-value = int(input("Enter the number:"))
-if (value % 2 ==0):
-    print("Entered number is not Prime Number")
+# 3.	Check for Palindrome
+# Write a program to check whether the given string is a palindrome or not using slicing.
+#******Using string reverse
+palindromevalues = input('Enter the value:')
+reversedstr =""
+for value in palindromevalues:
+    reversedstr =value+reversedstr
+if(palindromevalues == reversedstr):
+    print(f'This input string {palindromevalues} is a Palindrome')
 else:
-    print("Entered number is Prime Number")
+    print(f'This input string {palindromevalues} is not a Palindrome')
+
+#*******using reversed()
+
+palindromevalues = input('Enter the value:')
+reversedvalue =''.join(reversed(palindromevalues))
+if(palindromevalues == reversedvalue):
+    print(f'This input string {palindromevalues} is a Palindrome')
+else:
+    print(f'This input string {palindromevalues} is not a Palindrome')
